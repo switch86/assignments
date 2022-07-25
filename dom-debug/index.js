d1666a745125defb8b79fa296d752bfe953131a2
noah berlove <noah.berlove@gmail.com>
2022-07-25 14:33:01 -0700

    adding dom-debug assignment- completed

---
+++ dom-debug/index.js
@@ -0,0 +1,33 @@
+const colors = ["red", "blue", "green"];
+
+document.getElementById("add").addEventListener("click", function(){
+    const subItem = createSubItem();
+    document.getElementById("list").appendChild(subItem);
+})
+
+
+function createDropDown(){
+    const dropDown = document.createElement("select");
+    for (let i = 0; i < colors.length; i++){
+        var option = document.createElement("option");
+        option.append(colors[i]);
+        dropDown.append(option);
+    } 
+    dropDown.onchange = function changeColor() {
+            dropDown.parentElement.setAttribute("class", colors[dropDown.selectedIndex]); 
+        } 
+    return dropDown; 
+}
+   
+
+function createSubItem(){
+    const subItem = document.createElement("div");
+    var subItemValue = document.getElementById("input").value;
+    subItem.textContent = subItemValue;
+    const dropDown = createDropDown();
+    subItem.appendChild(dropDown);
+    subItem.setAttribute("class", "subItem");
+    return subItem;
+}
+
+
