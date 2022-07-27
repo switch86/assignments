9e7a72d71fe25231d1098b035a6b0c0bcaf02951
noah berlove <noah.berlove@gmail.com>
2022-07-26 19:38:50 -0700

    updated moms shopping list project

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -8,22 +8,55 @@ addItem.addEventListener("submit", (event) => {
     var newItem = addItem.newItem.value
     addItem.newItem.value = ""
     var li = document.createElement("li")
-    var div = document.createElement("div")
+    var itemText = document.createElement("div")
     var edit = document.createElement("button")
     var del = document.createElement("button")
     li.setAttribute("class", newItem)
     edit.setAttribute("class", "edit")
     del.setAttribute("class", "delete")
-    div.textContent = newItem
+    itemText.textContent = newItem
     edit.textContent = "Edit"
+    edit.addEventListener("click", (event) => {
+        event.preventDefault;
+        var editForm = document.createElement("form")
+        var editInput = document.createElement("input")
+        var saveButton = document.createElement("button")
+        saveButton.textContent = "save"
+        editForm.setAttribute("name", "editForm")
+        editInput.setAttribute("name", "editForm")
+        saveButton.addEventListener("submit", (event) => {
+            event.preventDefault;
+            itemText.textContent = editForm.input.value;
+            li.append(itemText);
+        })
+        edit.parentElement.append(editForm)
+        editForm.append(editInput)
+        editForm.append(saveButton)
+    });
     del.textContent = "X"
+    del.addEventListener("click", (event) => {
+        event.preventDefault;
+        del.parentElement.remove();
+    })
     ul.append(li)
-    li.append(div)
+    li.append(itemText)
     li.append(edit)
     li.append(del)
 })
 
-var removeItem = document.getElementsByClassName(".delete")
-removeItem.addEventListener("click", () => {
-    console.log("delete initiated")
-})
+// function save(event) => {
+//     event.preventDefault;
+//     itemText.textContent = editForm.value;
+//     li.append(itemText);
+// }
+// var removeItem = document.getElementsByClassName(".delete")
+// removeItem.addEventListener("click", () => {
+//     removeItem.parentElement.remove();
+// })
+// function edit() {
+//     var editForm = document.createElement("form")
+//     var editInput = document.createElement("input")
+//     var saveButton = document.createElement("button")
+//     saveButton.textContent = "save"
+//     var newValue = editInput.value
+// }
