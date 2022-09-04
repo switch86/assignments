11d47c6ae99da3cd6d2e5e911d8be957e4638102
noah berlove <noah.berlove@gmail.com>
2022-09-03 23:03:36 -0700

    updates - React

--- firstReact/app.js
+++ firstReact/app.js
@@ -1,6 +1,6 @@
 
 
-const navbar = (
+function navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,7 +36,7 @@ const navbar = (
                 </form>
             </div>
         </nav>
-    )
+}
 
 // Challenge: Create your own custom React component!
 // Call it "MainContent", and have it return a simple
@@ -47,7 +47,7 @@ const navbar = (
 
 
 ReactDOM.render(
-    navbar,
+    <navbar />,
     document.getElementById("root")
     
 )
