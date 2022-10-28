3e233562fc075e505bc179243d3a962f54c8d3cc
noah berlove <noah.berlove@gmail.com>
2022-10-28 12:10:25 -0400

    added assignment

--- memeGenerator/src/components/Meme.jsx
+++ memeGenerator/src/components/Meme.jsx
@@ -1,18 +1,33 @@
 import Post from './Post'
 import Text from "./Text"
-import Data from "../assets/Data"
+
 import React from "react"
+import axios from "axios"
 
 
+
+let memeHTML
 export default function Meme() {
-    let [memesArray, setMemesArray] = React.useState(Data.data.memes)
-    // let random = Math.floor(Math.random() * memesArray.length)
+    const [memeList, setMemeList] = React.useState([])
+    const [memesArray, setMemesArray] = React.useState({})
     const [memeImage, setMemeImage] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
+
+    React.useEffect(() => {
+        axios.get("https://api.imgflip.com/get_memes")
+            .then(res => res.data.data.memes)
+            .then(res => {
+                setMemesArray(res)
+            })
+            .catch(error => console.log(error))
+            }, [])
+        
     
+    // let random = Math.floor(Math.random() * memesArray.length)
+    // const [color, setColor] = React.useState(randomColor)
     function getMemeImage() {
         let random = Math.floor(Math.random() * memesArray.length)
         setMemeImage(prevState => {
@@ -32,18 +47,86 @@ export default function Meme() {
             }
         })
     }
+    function updateChange(num, name, value) {
+        // const {name, value} = event.target
+        console.log(num)
+        console.log(name)
+        console.log(value)
+        console.log(memeImage)
+        setMemeList(prevImage => {
+            if (prevImage.id = num) {
+                return {
+                    ...prevImage,
+                    [name]: value,
+                }
+            } else {
+                return {
+                    ...prevImage
+                }
+            }
+        })
+    }
+    
+    function saveMeme(event) {
+        event.preventDefault()
+        setMemeList(memeList => ([
+                memeImage,
+                ...memeList
+            ]))
+        console.log(memeList) 
+        }      
+            //     <div className="meme" onClick={deleteMeme}>
+            //     <img src={memeItem.memeImage}></img>
+            //     <h1 className="topText">{memeItem.topText}</h1>
+            //     <h1 className="bottomText">{memeItem.bottomText}</h1>        
+            // </div>
+                   
+    function deleteMeme(num) {
+        let arr = memeList.splice(num, 1)
+        setMemeList(arr)
+    }  
+
+    function editMeme(num) {
+        setMemeImage({...memeList[num]})
+        console.log(num)
+        console.log("edit meme")
+        console.log(memeImage)
+        
+    }
+    function saveNewMeme(num) {
+        console.log("save new meme")
+    }
+    let i = 0
+memeHTML = memeList.map(memeImage => {
+            i++
+            return (
+                <Post 
+                    key={i}
+                    id={i}
+                    topText={memeImage.topText}
+                    bottomText={memeImage.bottomText}
+                    memeImage={memeImage.randomImage}
+                    deleteMeme={deleteMeme}
+                    editMeme={editMeme}
+                    updateChange={updateChange}
+                    saveNewMeme={saveNewMeme}
+                />  
+            )
+        })
     return (
         <div className="memePage">
             <div className="MemeForm" name="Form">
                 <input name="topText" className="topText" placeholder="Top Text" onChange={handleChange} value={memeImage.topText}></input>
                 <input name="bottomText" className="bottomText" placeholder="Bottom Text" onChange={handleChange} value={memeImage.bottomText}></input>
                 <button onClick={getMemeImage} className="formButton">Get a new meme image</button>    
+                <button onClick={saveMeme} className="formButton">Save Meme</button>
             </div>
             <div className="meme">
                 <img src={memeImage.randomImage}></img>
                 <h1 className="topText">{memeImage.topText}</h1>
                 <h1 className="bottomText">{memeImage.bottomText}</h1>        
             </div>
+            {memeHTML}
         </div>
     )
 }
--- memeGenerator/src/components/Post.jsx
+++ memeGenerator/src/components/Post.jsx
@@ -1,10 +1,43 @@
-export default function Post(data) {
-    
+import React from "react"
+
+export default function Post(props) {
+    const [editing, setEditing] = React.useState(false) 
+    function deleteMeme(event) {
+        event.preventDefault()
+        props.deleteMeme(props.id)
+        }
+    function editMeme(event) {
+        event.preventDefault()
+        setEditing(true)
+        props.editMeme(props.id)
+        props.deleteMeme(props.id)
+    }
+    function updateChange(event) {
+        event.preventDefault()
+        props.updateChange(props.id, event.target.name, event.target.value)
+    }
+    function saveNewMeme(event) {
+        const {name, value} = event.target
+        event.preventDefault()
+        props.saveNewMeme(props.id)
+        setEditing(false)
+    }
     return (
-        <div className="Post">
-            <h1 className="topText"></h1>
-            <h2 className="bottomText"></h2>
-            <img src={data.url}></img>
+        <div className="meme">
+            <img src={props.memeImage}></img>
+            <h1 className="topText">{props.topText}</h1>
+            <h1 className="bottomText">{props.bottomText}</h1>
+            <p>{props.id}</p>
+            {editing ? 
+            <form>
+                <input name="topText" className="topText" placeholder="Top Text" onChange={updateChange} ></input>
+                <input name="bottomText" className="bottomText" placeholder="Bottom Text" onChange={updateChange} ></input>
+                <button className="formButton" onClick={saveNewMeme}>Save</button>
+            </form> : 
+            <div>
+                <button className="formButton" onClick={deleteMeme}>Delete</button>        
+                <button className="formButton" onClick={editMeme}>Edit</button>        
+            </div>}
         </div>
     )
 }
